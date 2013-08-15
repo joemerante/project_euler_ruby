494977ea8f48a46b0766e31c1b195bf47ee299aa
Joe Merante <merante.joe@gmail.com>
2013-08-14 20:48:12 -0700

    add problem statement to #4

--- largest_palindrome_product_spec.rb
+++ largest_palindrome_product_spec.rb
@@ -1,3 +1,6 @@
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
 require 'rspec'
 require_relative 'largest_palindrome_product'
 
